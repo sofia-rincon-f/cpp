459b684816a07476086afb9eae3bb7b4e347f482
sofia-rincon-f <s.rincon4@utp.edu.co>
2025-09-08 10:58:36 -0500

    Nuevo

--- condicionales.cpp
+++ condicionales.cpp
@@ -98,7 +98,82 @@ int main(){
 
 // Video 16 Ejercicio 4
 
-// Video 17 Ejercicio 5
+    int edad;
+
+    cout<<"Digite su edad: ";
+    cin>>edad;
+
+    //Rango 18 a 25
+
+    if ((edad>=18) && (edad<=25)){
+        cout<<"Su edad está en el rango de 18 a 25"
+    } else {
+        cout<<"Su edad no está en dicho rango"
+    }
+
+// Video 17 Ejercicio 5 Numero a romanos
+
+    int num, unidades, decenas, centenas, millar;
+
+    cout << "Digite un numero: ";
+    cin>>num; 
+
+    // Dividir numero=2152 ->2000+100+50+2
+
+    unidades = num%10; num /= 10; // unidades = 2, numero 215
+    decenas = num%10; numero /=10; //decenas=5, numero 21
+    centenas = num%10; numero /= 10;
+    millar = numero%10; numero /10;
+
+
+    switch (millar)
+    {
+    case 1: cout << "M"; break;
+    case 2: cout << "MM"; break;
+    case 3: cout << "MMM"; break;
+    }
+
+    switch (centenas)
+    {
+    case 1: cout << "C"; break;
+    case 2: cout << "CC"; break;
+    case 3: cout << "CCC"; break;
+    case 4: cout << "CD"; break;
+    case 5: cout << "D"; break;
+    case 6: cout << "DC"; break;
+    case 7: cout << "DCC"; break;
+    case 8: cout << "DCCC"; break;
+    case 9: cout << "CM"; break;
+    }
+
+    switch (decenas)
+    {
+    case 1: cout << "X"; break;
+    case 2: cout << "XX"; break;
+    case 3: cout << "XXX"; break;
+    case 4: cout << "XL"; break;
+    case 5: cout << "L"; break;
+    case 6: cout << "LX"; break;
+    case 7: cout << "LXX"; break;
+    case 8: cout << "LXXX"; break;
+    case 9: cout << "XC"; break;
+    }
+
+    
+    switch (unidades)
+    {
+    case 1: cout << "I"; break;
+    case 2: cout << "II"; break;
+    case 3: cout << "III"; break;
+    case 4: cout << "IV"; break;
+    case 5: cout << "V"; break;
+    case 6: cout << "VI"; break;
+    case 7: cout << "VII"; break;
+    case 8: cout << "VIII"; break;
+    case 9: cout << "IX"; break;
+    }
+
+
 
 // Video 18 Ejercicio 6
 
