d41a70b817fd88d55b8994f02661991291257895
Sofia <s.rincon4@utp.edu.co>
2025-09-08 10:41:55 -0500

    Add files via upload

---
+++ pedir_datos.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // Video 5
+
+    int edad; 
+    char sexo[10];
+    float altura;
+
+
+    cout<<"Digite su edad "; cin>>edad;
+    cout<<"Digite su sexo ";cin>>sexo;
+    cout<<"Digite su altura";cin>>altura;
+
+
+    cout<<"\nEdad: "<<edad<<endl;
+    cout<<"\nSexo: "<<sexo<<endl;
+    cout<<"\nAltura: "<<altura<<endl;
+
+    return 0;
+}
---
+++ tipos_ddatos.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std; 
+
+int main (){
+
+    // Video 1
+
+    cout << "Hola mundo :D"<<endl;
+
+    //video 2 Datos basicos 
+
+    int entero = 15;
+    float flotante = 10.45;
+    double mayor = 16.3456;
+    char letra = 'a';
+
+    cout << entero << endl;
+
+    // Video 3 Estrada y salida
+
+
+    int numero;
+
+    cout << "Digite un numero: ";
+    cin >> numero;
+
+    cout << "\nEl numero que digito es " << numero;
+
+    /*
+    Comentario de varias lineas 
+    */
+
+    return 0;
+}
